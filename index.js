bd097c65ad9a5819362eb2a8cecb5776597f716d
Konstantin Mitckevich <konstantin.mitskevich@schibsted.com>
2017-10-06 14:40:34 +0100

    First files

---
+++ index.js
@@ -0,0 +1,7 @@
+let getEverySecondLetter = require('./js/first.js');
+
+const text = " H e l l o   W o r l d !";
+
+const processedText = getEverySecondLetter(text);
+
+console.log(processedText);
---
+++ js/first.js
@@ -0,0 +1,11 @@
+/**
+ * Get Array with every second letter of string from text param.
+ */
+function get(text){
+    let array = Array(text.length/2);
+    for(let i=0;i<text.length/2;i++){
+        array[i]=text[i*2+1];
+    }
+    return array;}
+
+module.exports = get;
