7ad75039fbb7ceb772d20e9d9c6c85a6debb113e
tyger <tyger@users.noreply.github.com>
2017-10-09 16:29:17 +0100

    Median (#4)
    * Median 1
    
    * Filter method

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ const array = [1,2,3,4,8,4,9,0,3,29];
 const resultAverage = secondLib.average(array);
 
 console.log(`Average of [${array}] is ${resultAverage}`);
+
+const resultMedian = secondLib.median(array);
+
+console.log(`Result median = ${resultMedian}`);
--- js/second.js
+++ js/second.js
@@ -1,4 +1,17 @@
 function calculateAverage(array) {
     return array.reduce((acc, item) => acc + item, 0)/array.length;
 }
+
+function calculateMedian(array) {
+    return array.sort((a, b) => a - b)[array.length / 2];
+}
+
+function calcFilter(array) {
+    var av = calculateAverage(array);
+    var med = calculateMedian(array);
+    return array.filter(item => item > Math.min(av,med) && item <= Math.max(av,med));
+}
+
+module.exports.median = calculateMedian;
 module.exports.average = calculateAverage;
+module.exports.filter = calcFilter;
