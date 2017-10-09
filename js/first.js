4dace986584bc58a3f3d0915eaf2696cc187bdfe
Konstantin Mitckevich <konstantin.mitskevich@schibsted.com>
2017-10-09 15:41:26 +0100

    Styling

--- js/first.js
+++ js/first.js
@@ -1,15 +1,13 @@
-function get(text)
-{
+function get(text) {
     /**
      * Get Array with every second letter of string from text param.
      */
-    let array = Array(text.length/2);
-    for(let i=0;i<text.length/2;i++){array[i]=text[i*2+1];}
-    return array;}
-
-
-
-
+    let array = Array(text.length / 2);
+    for (let i = 0; i < text.length / 2; i++) {
+        array[i] = text[i * 2 + 1];
+    }
+    return array;
+}
 
 
 module.exports = get;
