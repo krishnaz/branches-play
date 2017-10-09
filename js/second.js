c828639b10c537a306878f172c97689fdf4a19a6
tyger <tyger@users.noreply.github.com>
2017-10-09 16:23:21 +0100

    REfactor average (#3)
    * REfactor average
    
    * Average fix

--- js/second.js
+++ js/second.js
@@ -1,8 +1,4 @@
 function calculateAverage(array) {
-    let acc = 0;
-    for(let i=0; i<array.length; i++){
-        acc += array[i];
-    }
-    return acc/array.length;
+    return array.reduce((acc, item) => acc + item, 0)/array.length;
 }
 module.exports.average = calculateAverage;
